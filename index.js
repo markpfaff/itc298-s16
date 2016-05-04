f97bafdab0e5e758bd00efb284263d6c93c3e06f
markpfaff <markdpfaff@gmail.com>
2016-05-03 22:47:42 -0700

    Adds detail view, partials, date widget

--- index.js
+++ index.js
@@ -19,6 +19,17 @@ app.use(bodyParser.urlencoded({ extended: true})); //support encoded bodies
 //create public folder for images,css etc
 app.use(express.static('public'));
 
+function getDate(){
+    return Date();
+}
+
+//date partial
+app.use(function(req, res, next){
+    if(!res.locals.partials) res.locals.partials = {};
+    res.locals.partials.currentDate = getDate();
+    next();
+});
+
 app.get('/', function(req, res) {
     res.type('html');
 
@@ -28,8 +39,18 @@ app.get('/', function(req, res) {
 
 });
 
+//detail view
+app.get('/detail/:artistName', function(req, res){
+    var userArtist = req.params.artistName.toLowerCase();
+    var userArtistResult = artist.searchArray(userArtist);
+    var newDate = false;
+    if (userArtistResult.date > '2015-01-01'){
+        newDate = true;
+    }
+    res.render('detail', {page_title: userArtist, title:req.params.artistName, track: userArtistResult.track, date:userArtistResult.date, newDate:newDate});
+});
 
-
+//simple about page
 app.get('/about', function(req, res) {
     res.render('about');
 });
@@ -39,6 +60,7 @@ app.post('/search', function(req,res){
     res.type('html');
     var page_title = 'Searching for: ' + req.body.search_term;
     var user_search_term = req.body.search_term;
+
 
     if (artist.searchArray(user_search_term)){
 
@@ -79,18 +101,18 @@ app.post('/add', function(req,res){
 //add an item to the list
 app.post('/update', function(req,res){
     res.type('html');
-    var page_title = 'Updating: ' + req.body.user_existing_artist_name + ' to ' + req.body.user_new_artist_name;
-
+    var page_title = 'Updating: ' + req.body.user_new_artist_name;
+    var artist_name = req.body.user_new_artist_name;
     var user_new_artist = {name: req.body.user_new_artist_name, track: req.body.user_new_artist_track, date: req.body.user_new_artist_date };
 
-    artist.updateArtist( user_new_artist );
+    var found = artist.updateArtist( user_new_artist );
 
-    if (artist.updateArtist().found == true){
+    if (found === true){
 
-        res.render('results', {title:'Update Results', page_title:artist.updateArtist().enteredArtistName, results:'Success ' + req.body.user_existing_artist_name + ' has been updated! There are ' + artist.showArrayLength() + ' artists total'});
+        res.render('results', {page_title:artist_name, results:'Success ' + artist_name + ' has been updated! There are ' + artist.showArrayLength() + ' artists total'});
 
     }else{
-        res.render('results', {title:'Update Results', page_title:artist.updateArtist().enteredArtistName, results:'Unable to update artist'});
+        res.render('results', {page_title:artist_name, results:'Unable to update artist'});
     }
 
 
