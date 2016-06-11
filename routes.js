33e8e57eef821b4fcd97c40e2baba403c12f01b1
markpfaff <markdpfaff@gmail.com>
2016-06-10 22:51:31 -0700

    Converts app to one-page functionality

--- routes.js
+++ routes.js
@@ -59,36 +59,32 @@ module.exports = function(app){
 
 	//remove an artist from the database
 	app.post('/remove', function(req,res){
-	    res.type('html');
-	    var page_title = 'Removing: ' + req.body.remove_term;
-	    var user_remove_term = req.body.remove_term.toLowerCase();
 
+	    var user_remove_term = req.body.name.toLowerCase();
 	    ArtistDB.findOneAndRemove({name: user_remove_term} , function(err,artist){
 	    	if(err) return console.log(err);
 		    if (artist){
 
-		        res.render('results', {title:'Search Results', page_title:page_title, results:'Success! ' + user_remove_term + ' has been removed.'} );
+		    res.json({"result":"Success! Artist has been removed"});
 
-		    } else {
-		        res.render('results', {title:'Search Results', page_title:page_title, results:'Couldn\'t find artist to remove'} );
+			} else {
+			res.json({"result":"Woops! Artist doesn't exist!"});
 
-		    }
+	    	}
 	    });
 
 	});
 
 	//add an artist to the database
 	app.post('/add', function(req,res){
-	    res.type('html');
-	    var page_title = 'Adding: ' + req.body.add_name + ' / ' + req.body.add_track + ' / ' + req.body.add_date;
-	    var user_add_artist = [{name: req.body.add_name.toLowerCase(), track: req.body.add_track.toLowerCase(), date: req.body.add_date}];
-	    var addArtistName = req.body.add_name.toLowerCase();
-	    var addArtistTrack = req.body.artist.track.toLowerCase();
-		var addArtistDate = req.body.add_date;
+		console.log(req.body);
+	    var addArtistName = req.body.name.toLowerCase();
+	    var addArtistTrack = req.body.track.toLowerCase();
+		var addArtistDate = req.body.date;
 
 		var found = false;
 
-		ArtistDB.findOne({name: user_add_artist.name}, function(err, artist){
+		ArtistDB.findOne({name: artist.name}, function(err, artist){
 				if(err) return console.log(err);
 
 				found = true;
@@ -98,16 +94,17 @@ module.exports = function(app){
 		if(found == false){
 
 	        var addArtist = new ArtistDB({
-	           name: addArtistName,
-	           track: addArtistTrack,
-	           date: addArtistDate
-	        }).save(function(err){
-            if(err) return console.log(err);
+		           name: addArtistName,
+		           track: addArtistTrack,
+		           date: addArtistDate
+		    }).save(function(err){
+	            if(err) res.send(err);
         	});
 
-	        res.render('results', {title:'Add Results', page_title:page_title, results:'Success ' + req.body.add_name + ' has been added! There are ' + ArtistDB.length + ' artists total'});
-		}else{
-			res.render('results', {title:'Add Results', page_title:page_title, results:'Artist not added. Artist already exists.'});	    	
+        	res.json({"result":"Success! Artist has been added"});
+
+		} else {
+			res.json({"result":"Woops! Artist already exists!"});
 
 		}
 
@@ -116,27 +113,24 @@ module.exports = function(app){
 
 	//update an artist
 	app.post('/update', function(req,res){
-	    res.type('html');
-	    var page_title = 'Updating: ' + req.body.user_new_artist_name;
-	    var artist_name = req.body.user_new_artist_name.toLowerCase();
-	    var user_new_artist = {name: req.body.user_new_artist_name.toLowerCase(), track: req.body.user_new_artist_track.toLowerCase(), date: req.body.user_new_artist_date };
+	    var artist_name = req.body.name.toLowerCase();
+	    var user_new_artist = {name: req.body.name.toLowerCase(), track: req.body.track.toLowerCase(), date: req.body.date };
 
-	 ArtistDB.findOne(artist_name, function (err, artist) {
-	      if (err) return console.log(err);
-	      
-		      artist.name= user_new_artist.name;
-		      artist.track = user_new_artist.track;
-		      artist.date = user_new_artist.date;
-		      artist.save(function (err) {
-		        if (err) return console.log(err);
+		 ArtistDB.findOne(artist_name, function (err, artist) {
+		      if (err) return console.log(err);
 
-		        res.render('results', {page_title:artist_name, results:'Success ' + artist_name + ' has been updated! There are ' + ArtistDB.length + ' artists total'});
+			      artist.name = user_new_artist.name;
+			      artist.track = user_new_artist.track;
+			      artist.date = user_new_artist.date;
+			      artist.save(function (err) {
+			        if (err) return console.log(err);
 
-		        });
+	        		res.json({"result":"Success! Artist has been updated"});
 
-	      });
+					});
 
 
+		});
 	});
 
 	//api for all artist
